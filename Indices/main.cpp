4f035cd6410e6afadfc714b59efc147fd1bdd266
abbiebarnhardt19 <abbiebarnhardt@gmail.com>
2026-01-07 16:17:39 -0600

    Updated references

--- Indices/main.cpp
+++ Indices/main.cpp
@@ -9,7 +9,7 @@ int main() {
     int target = 11;
     vector<int> nums = {3,2, 8};
     int test[3] = {1, 2, 3};
-    //getNumSums(nums, target);
+
     std::pair<int, int> result = getNumSums(nums, target, test);
     std::cout << "Result: [" << result.first << "," << result.second << "]" << std::endl;
     
